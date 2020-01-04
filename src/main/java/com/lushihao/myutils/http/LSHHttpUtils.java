d74fe664969d46e0c634510b1a367a30ec263793
lushihao <lushihaomb@qq.com>
2020-01-04 15:03:40 +0800

    更新工具类

--- src/main/java/com/lushihao/myutils/http/LSHHttpUtils.java
+++ src/main/java/com/lushihao/myutils/http/LSHHttpUtils.java
@@ -1,286 +1,52 @@
 package com.lushihao.myutils.http;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Map;
-import java.util.Set;
 
 public final class LSHHttpUtils {
 
     /**
-     * 编码格式
-     */
-    public static final String UTF8 = "UTF-8";
-    /**
-     * 编码格式
-     */
-    public static final String GBK = "GBK";
-
-    /**
      * 支持的Http method
      */
-    private enum HttpMethod {
+    public enum HttpMethod {
         POST, DELETE, GET, PUT, HEAD
     }
 
     /**
-     * POST方法提交Http请求，语义为“增加” <br/>
-     * 注意：Http方法中只有POST方法才能使用body来提交内容
+     * 请求
      *
-     * @param url            资源路径（如果url中已经包含参数，则params应该为null）
-     * @param params         参数
-     * @param connectTimeout 连接超时时间（单位为ms）
-     * @param readTimeout    读取超时时间（单位为ms）
-     * @param charset        字符集（一般该为“utf-8”）
-     * @return
-     */
-    public static String post(String url, Map params, int connectTimeout, int readTimeout, String charset) {
-        return invokeUrl(url, params, null, connectTimeout, readTimeout, charset, HttpMethod.POST);
-    }
-
-    /**
-     * POST方法提交Http请求，语义为“增加” <br/>
-     * 注意：Http方法中只有POST方法才能使用body来提交内容
-     *
-     * @param url            资源路径（如果url中已经包含参数，则params应该为null）
-     * @param params         参数
-     * @param headers        请求头参数
-     * @param connectTimeout 连接超时时间（单位为ms）
-     * @param readTimeout    读取超时时间（单位为ms）
-     * @param charset        字符集（一般该为“utf-8”）
-     * @return
-     */
-    public static String post(String url, Map params, Map<String, String> headers, int connectTimeout, int readTimeout, String charset) {
-        return invokeUrl(url, params, headers, connectTimeout, readTimeout, charset, HttpMethod.POST);
-    }
-
-    /**
-     * GET方法提交Http请求，语义为“查询”
-     *
-     * @param url            资源路径（如果url中已经包含参数，则params应该为null）
-     * @param params         参数
-     * @param connectTimeout 连接超时时间（单位为ms）
-     * @param readTimeout    读取超时时间（单位为ms）
-     * @param charset        字符集（一般该为“utf-8”）
-     * @return
-     */
-    public static String get(String url, Map params, int connectTimeout, int readTimeout, String charset) {
-        return invokeUrl(url, params, null, connectTimeout, readTimeout, charset, HttpMethod.GET);
-    }
-
-    /**
-     * GET方法提交Http请求，语义为“查询”
-     *
-     * @param url            资源路径（如果url中已经包含参数，则params应该为null）
-     * @param params         参数
-     * @param headers        请求头参数
-     * @param connectTimeout 连接超时时间（单位为ms）
-     * @param readTimeout    读取超时时间（单位为ms）
-     * @param charset        字符集（一般该为“utf-8”）
-     * @return
-     */
-    public static String get(String url, Map params, Map<String, String> headers, int connectTimeout, int readTimeout, String charset) {
-        return invokeUrl(url, params, headers, connectTimeout, readTimeout, charset, HttpMethod.GET);
-    }
-
-    /**
-     * PUT方法提交Http请求，语义为“更改” <br/>
-     * 注意：PUT方法也是使用url提交参数内容而非body，所以参数最大长度收到服务器端实现的限制，Resin大概是8K
-     *
-     * @param url            资源路径（如果url中已经包含参数，则params应该为null）
-     * @param params         参数
-     * @param connectTimeout 连接超时时间（单位为ms）
-     * @param readTimeout    读取超时时间（单位为ms）
-     * @param charset        字符集（一般该为“utf-8”）
-     * @return
-     */
-    public static String put(String url, Map params, int connectTimeout, int readTimeout, String charset) {
-        return invokeUrl(url, params, null, connectTimeout, readTimeout, charset, HttpMethod.PUT);
-    }
-
-    /**
-     * PUT方法提交Http请求，语义为“更改” <br/>
-     * 注意：PUT方法也是使用url提交参数内容而非body，所以参数最大长度收到服务器端实现的限制，Resin大概是8K
-     *
-     * @param url            资源路径（如果url中已经包含参数，则params应该为null）
-     * @param params         参数
-     * @param headers        请求头参数
-     * @param connectTimeout 连接超时时间（单位为ms）
-     * @param readTimeout    读取超时时间（单位为ms）
-     * @param charset        字符集（一般该为“utf-8”）
-     * @return
-     */
-    public static String put(String url, Map params, Map<String, String> headers, int connectTimeout, int readTimeout, String charset) {
-        return invokeUrl(url, params, headers, connectTimeout, readTimeout, charset, HttpMethod.PUT);
-    }
-
-    /**
-     * DELETE方法提交Http请求，语义为“删除”
-     *
-     * @param url            资源路径（如果url中已经包含参数，则params应该为null）
-     * @param params         参数
-     * @param connectTimeout 连接超时时间（单位为ms）
-     * @param readTimeout    读取超时时间（单位为ms）
-     * @param charset        字符集（一般该为“utf-8”）
-     * @return
-     */
-    public static String delete(String url, Map params, int connectTimeout, int readTimeout, String charset) {
-        return invokeUrl(url, params, null, connectTimeout, readTimeout, charset, HttpMethod.DELETE);
-    }
-
-    /**
-     * DELETE方法提交Http请求，语义为“删除”
-     *
-     * @param url            资源路径（如果url中已经包含参数，则params应该为null）
-     * @param params         参数
-     * @param headers        请求头参数
-     * @param connectTimeout 连接超时时间（单位为ms）
-     * @param readTimeout    读取超时时间（单位为ms）
-     * @param charset        字符集（一般该为“utf-8”）
-     * @return
-     */
-    public static String delete(String url, Map params, Map<String, String> headers, int connectTimeout, int readTimeout, String charset) {
-        return invokeUrl(url, params, headers, connectTimeout, readTimeout, charset, HttpMethod.DELETE);
-    }
-
-    /**
-     * HEAD方法提交Http请求，语义同GET方法  <br/>
-     * 跟GET方法不同的是，用该方法请求，服务端不返回message body只返回头信息，能节省带宽
-     *
-     * @param url            资源路径（如果url中已经包含参数，则params应该为null）
-     * @param params         参数
-     * @param connectTimeout 连接超时时间（单位为ms）
-     * @param readTimeout    读取超时时间（单位为ms）
-     * @param charset        字符集（一般该为“utf-8”）
-     * @return
-     */
-    public static String head(String url, Map params, int connectTimeout, int readTimeout, String charset) {
-        return invokeUrl(url, params, null, connectTimeout, readTimeout, charset, HttpMethod.HEAD);
-    }
-
-    /**
-     * HEAD方法提交Http请求，语义同GET方法  <br/>
-     * 跟GET方法不同的是，用该方法请求，服务端不返回message body只返回头信息，能节省带宽
-     *
-     * @param url            资源路径（如果url中已经包含参数，则params应该为null）
-     * @param params         参数
-     * @param headers        请求头参数
-     * @param connectTimeout 连接超时时间（单位为ms）
-     * @param readTimeout    读取超时时间（单位为ms）
-     * @param charset        字符集（一般该为“utf-8”）
-     * @return
-     */
-    public static String head(String url, Map params, Map<String, String> headers, int connectTimeout, int readTimeout, String charset) {
-        return invokeUrl(url, params, headers, connectTimeout, readTimeout, charset, HttpMethod.HEAD);
-    }
-
-    /**
-     * 请求数据
-     *
-     * @param url
-     * @param params
-     * @param headers
+     * @param strURL
      * @param connectTimeout
      * @param readTimeout
-     * @param encoding
      * @param method
      * @return
      */
-    private static String invokeUrl(String url, Map params, Map<String, String> headers, int connectTimeout, int readTimeout, String encoding, HttpMethod method) {
-        //构造请求参数字符串
-        StringBuilder paramsStr = null;
-        if (params != null) {
-            paramsStr = new StringBuilder();
-            Set<Map.Entry> entries = params.entrySet();
-            for (Map.Entry entry : entries) {
-                String value = (entry.getValue() != null) ? (String.valueOf(entry.getValue())) : "";
-                paramsStr.append(entry.getKey() + "=" + value + "&");
-            }
-            if (paramsStr != null && paramsStr.toString() != "") {
-                paramsStr = new StringBuilder(paramsStr.toString().substring(0, paramsStr.length() - 2));
-            }
-            //只有POST方法才能通过OutputStream(即form的形式)提交参数
-            if (method != HttpMethod.POST) {
-                url += "?" + paramsStr.toString();
-            }
-        }
-
-        URL uUrl = null;
-        HttpURLConnection conn = null;
-        BufferedWriter out = null;
-        BufferedReader in = null;
+    public static String request(String strURL, int connectTimeout, int readTimeout, HttpMethod method) {
+        URL url = null;
+        HttpURLConnection httpConn = null;
+        StringBuffer buffer = null;
         try {
-            //创建和初始化连接
-            uUrl = new URL(url);
-            conn = (HttpURLConnection) uUrl.openConnection();
-            conn.setRequestProperty("content-type", "application/x-www-form-urlencoded");
-            conn.setRequestMethod(method.toString());
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            //设置连接超时时间
-            conn.setConnectTimeout(connectTimeout);
-            //设置读取超时时间
-            conn.setReadTimeout(readTimeout);
-            //指定请求header参数
-            if (headers != null && headers.size() > 0) {
-                Set<String> headerSet = headers.keySet();
-                for (String key : headerSet) {
-                    conn.setRequestProperty(key, headers.get(key));
-                }
-            }
+            url = new URL(strURL);
+            httpConn = (HttpURLConnection) url.openConnection();
+            httpConn.setRequestMethod(method.toString());
+            httpConn.setConnectTimeout(connectTimeout);
+            httpConn.setReadTimeout(readTimeout);
+            httpConn.connect();
 
-            if (paramsStr != null && method == HttpMethod.POST) {
-                //发送请求参数
-                out = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream(), encoding));
-                out.write(paramsStr.toString());
-                out.flush();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
+            String line;
+            buffer = new StringBuffer();
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
             }
-
-            //接收返回结果
-            StringBuilder result = new StringBuilder();
-            in = new BufferedReader(new InputStreamReader(conn.getInputStream(), encoding));
-            if (in != null) {
-                String line = "";
-                while ((line = in.readLine()) != null) {
-                    result.append(line);
-                }
-            }
-            return result.toString();
+            reader.close();
+            httpConn.disconnect();
         } catch (Exception e) {
-            //处理错误流，提高http连接被重用的几率
-            try {
-                byte[] buf = new byte[100];
-                InputStream es = conn.getErrorStream();
-                if (es != null) {
-                    while (es.read(buf) > 0) {
-                        ;
-                    }
-                    es.close();
-                }
-            } catch (Exception e1) {
-                e1.printStackTrace();
-            }
-        } finally {
-            try {
-                if (out != null) {
-                    out.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            try {
-                if (in != null) {
-                    in.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            //关闭连接
-            if (conn != null) {
-                conn.disconnect();
-            }
+            e.printStackTrace();
         }
-        return null;
+
+        return buffer.toString();
     }
 }
