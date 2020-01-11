89bd13e4d5f8618af62e137b6a73e00c8be3d439
lushihao <lushihaomb@qq.com>
2020-01-11 18:56:43 +0800

    更新json工具类

--- src/main/java/com/lushihao/myutils/json/LSHJsonUtils.java
+++ src/main/java/com/lushihao/myutils/json/LSHJsonUtils.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 
 import java.util.List;
 import java.util.Map;
@@ -37,7 +38,7 @@ public final class LSHJsonUtils {
      * @return JSON数据
      */
     public static String bean2Json(Object object) {
-        return JSON.toJSONString(object);
+        return JSON.toJSONString(object, SerializerFeature.DisableCircularReferenceDetect);
     }
 
     /**
